9997bb9d382abca90c8e5a5c6de56c4e6607a21e
Lucas Melo <lucas_santosmelo@hotmail.com>
2024-04-03 23:57:55 -0300

    Aula 05 - Typescript

---
+++ Aula 05/email.ts
@@ -0,0 +1,26 @@
+class ServicoEmail {
+    enviarEmail(mensagem: string){
+        console.log("Enviando e-mail:", mensagem);
+    }
+}
+
+class UsuarioService{
+
+    private servicoEmail: ServicoEmail;
+
+    constructor(servicoEmail: ServicoEmail){
+        this.servicoEmail = servicoEmail;
+    }
+
+    registrarUsuario(usuario: { nome: string }){
+
+        console.log(`Registrando o usuário ${usuario.nome }`);
+        this.servicoEmail.enviarEmail(`Bem vindo ${usuario.nome } ao nosso serviço!`);
+    }
+}
+
+const servicoEmail = new ServicoEmail();
+const usuarioService = new UsuarioService(servicoEmail);
+
+const usuario = { nome: 'Lucas'};
+usuarioService.registrarUsuario(usuario);
